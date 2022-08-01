2fcb0d4c839dabb130dc68335cfce85fbdc6533d
Jessica Wang <jessica.l.wang01@gmail.com>
2022-08-01 19:55:57 -0400

    deploying

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,4 +1,4 @@
-// Define the query and mutation functionality to work with the Mongoose models.
+// Define the query and mutation functionality to work with the Mongoose models
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Rx } = require('../models');
 const { signToken } = require("../utils/auth");
